bddfa2e3be5ee715dede70ce0eb32b9fd180f599
Nawakorn23 <nawakorn.c@gmail.com>
2024-04-16 01:11:40 +0700

    fix review/edit/page and edit about us page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,55 +1,332 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function AboutPage(){
+export default function AboutPage() {
+  return (
+    <main>
+      <div className="block pt-16 pl-20">
+        <div className="text-[#5577CE] font-bold text-[25px]">
+          Chananut Hussarung
+        </div>
+        <div className="flex flex-row px-9 py-4">
+          <Image
+            src={"/img/github.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            <a href="https://github.com/sherlyzi" className="hover:underline">
+              sherlyzi
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row px-9 py-px">
+          <Image
+            src={"/img/email.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            6633045521@student.chula.ac.th
+          </div>
+        </div>
+      </div>
 
-   return(
-      <main>
-         <div className='block pt-16 pl-20'>
-            <div className='text-[#5577CE] font-bold text-[25px]'>Chananut Hussarung</div>
-            <div className='flex flex-row px-9 py-4'>
-               <Image src={'/img/github.png'} className='h-full w-auto' alt='logo'
-               width={0} height={0} sizes='55vh'/>
-               <div className='text-[17px] ml-6 text-center mt-auto mb-auto'><a href='https://github.com/sherlyzi'>sherlyzi</a></div> 
-            </div>
-            <div className='flex flex-row px-9 py-px'>
-               <Image src={'/img/email.png'} className='h-full w-auto' alt='logo'
-               width={0} height={0} sizes='55vh'/>
-               <div className='text-[17px] ml-6 text-center mt-auto mb-auto'>6633045521@student.chula.ac.th</div>
-            </div>
-         </div>
+      <div className="block pt-16 pl-20">
+        <div className="text-[#5577CE] font-bold text-[25px]">
+          Nawakorn Chamroon
+        </div>
+        <div className="flex flex-row px-9 py-4">
+          <Image
+            src={"/img/github.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            <a href="https://github.com/Nawakorn23" className="hover:underline">
+              Nawakorn23
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row px-9 py-px">
+          <Image
+            src={"/img/email.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            6633063821@student.chula.ac.th
+          </div>
+        </div>
+      </div>
 
-         <div className='block pt-16 pl-20'>
-            <div className='text-[#5577CE] font-bold text-[25px]'>Nawakorn Chamroon</div>
-            <div className='flex flex-row px-9 py-4'>
-               <Image src={'/img/github.png'} className='h-full w-auto' alt='logo'
-               width={0} height={0} sizes='55vh'/>
-               <div className='text-[17px] ml-6 text-center mt-auto mb-auto'><a href='https://github.com/Nawakorn23'>Nawakorn23</a></div> 
-            </div>
-            <div className='flex flex-row px-9 py-px'>
-               <Image src={'/img/email.png'} className='h-full w-auto' alt='logo'
-               width={0} height={0} sizes='55vh'/>
-               <div className='text-[17px] ml-6 text-center mt-auto mb-auto'>6633063821@student.chula.ac.th</div>
-            </div>
-         </div>
+      <div className="block pt-16 pl-20">
+        <div className="text-[#5577CE] font-bold text-[25px]">
+          Nitchakarn Jirathunyawong
+        </div>
+        <div className="flex flex-row px-9 py-4">
+          <Image
+            src={"/img/github.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            <a
+              href="https://github.com/NitchakarnJ"
+              className="hover:underline"
+            >
+              NitchakarnJ
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row px-9 py-px">
+          <Image
+            src={"/img/email.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            6633077621@student.chula.ac.th
+          </div>
+        </div>
+      </div>
 
-         <div className='block pt-16 pl-20'>
-            <div className='text-[#5577CE] font-bold text-[25px]'>Nitchakarn Jirathunyawong</div>
-            <div className='flex flex-row px-9 py-4'>
-               <Image src={'/img/github.png'} className='h-full w-auto' alt='logo'
-               width={0} height={0} sizes='55vh'/>
-               <div className='text-[17px] ml-6 text-center mt-auto mb-auto'><a href='https://github.com/NitchakarnJ'>NitchakarnJ</a></div> 
-            </div>
-            <div className='flex flex-row px-9 py-px'>
-               <Image src={'/img/email.png'} className='h-full w-auto' alt='logo'
-               width={0} height={0} sizes='55vh'/>
-               <div className='text-[17px] ml-6 text-center mt-auto mb-auto'>6633077621@student.chula.ac.th</div>
-            </div>
-         </div>
-         <div className='block pt-16'>
-            <Image src={'/img/about.jpg'} alt='about' width={5000} height={5000} sizes='100vh' objectFit='cover'/>
-         </div>
-      </main>
-   )
+      <div className="block pt-16 pl-20">
+        <div className="text-[#5577CE] font-bold text-[25px]">
+          Nattawit Manathumrong
+        </div>
+        <div className="flex flex-row px-9 py-4">
+          <Image
+            src={"/img/github.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            <a
+              href="https://github.com/NattawitMana"
+              className="hover:underline"
+            >
+              NattawitMana
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row px-9 py-px">
+          <Image
+            src={"/img/email.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            6633066721@student.chula.ac.th
+          </div>
+        </div>
+      </div>
 
+      <div className="block pt-16 pl-20">
+        <div className="text-[#5577CE] font-bold text-[25px]">
+          Ronnakorn Pitinanon
+        </div>
+        <div className="flex flex-row px-9 py-4">
+          <Image
+            src={"/img/github.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            <a href="https://github.com/UnicornZ55" className="hover:underline">
+              UnicornZ55
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row px-9 py-px">
+          <Image
+            src={"/img/email.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            6633204021@student.chula.ac.th
+          </div>
+        </div>
+      </div>
 
+      <div className="block pt-16 pl-20">
+        <div className="text-[#5577CE] font-bold text-[25px]">
+          Siamrat Srithawatphong
+        </div>
+        <div className="flex flex-row px-9 py-4">
+          <Image
+            src={"/img/github.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            <a href="https://github.com/Siamrat27" className="hover:underline">
+              Siamrat27
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row px-9 py-px">
+          <Image
+            src={"/img/email.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            6633204021@student.chula.ac.th
+          </div>
+        </div>
+      </div>
+
+      <div className="block pt-16 pl-20">
+        <div className="text-[#5577CE] font-bold text-[25px]">
+          Natkamon Maleehuan
+        </div>
+        <div className="flex flex-row px-9 py-4">
+          <Image
+            src={"/img/github.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            <a href="https://github.com/agehcx" className="hover:underline">
+              agehcx
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row px-9 py-px">
+          <Image
+            src={"/img/email.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            6633068021@student.chula.ac.th
+          </div>
+        </div>
+      </div>
+
+      <div className="block pt-16 pl-20">
+        <div className="text-[#5577CE] font-bold text-[25px]">
+          Ameen Chebueraheng
+        </div>
+        <div className="flex flex-row px-9 py-4">
+          <Image
+            src={"/img/github.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            <a href="https://github.com/Aftermin" className="hover:underline">
+              Aftermin
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row px-9 py-px">
+          <Image
+            src={"/img/email.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            6633289321@student.chula.ac.th
+          </div>
+        </div>
+      </div>
+
+      <div className="block pt-16 pl-20">
+        <div className="text-[#5577CE] font-bold text-[25px]">Daniel Samae</div>
+        <div className="flex flex-row px-9 py-4">
+          <Image
+            src={"/img/github.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            <a
+              href="https://github.com/danielza6655"
+              className="hover:underline"
+            >
+              danielza6655
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row px-9 py-px">
+          <Image
+            src={"/img/email.png"}
+            className="h-full w-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="55vh"
+          />
+          <div className="text-[17px] ml-6 text-center mt-auto mb-auto">
+            6633078221@student.chula.ac.th
+          </div>
+        </div>
+      </div>
+
+      <div className="block pt-16">
+        <Image
+          src="/img/about.jpg"
+          alt="about"
+          width={5000}
+          height={5000}
+          layout="responsive"
+          objectFit="cover"
+          className="w-full h-auto"
+        />
+      </div>
+    </main>
+  );
 }
--- frontend/src/app/review/edit/[rid]/page.tsx
+++ frontend/src/app/review/edit/[rid]/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 import editReview from "@/libs/editReview";
 import { ReviewItemEdit } from "../../../../../interface";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { FormEvent, useState } from "react";
-import * as React from 'react';
+import * as React from "react";
 import editUserProfile from "@/libs/editUserProfile";
 import { useSession } from "next-auth/react";
 import TextField from "@mui/material/TextField";
@@ -13,116 +13,114 @@ import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
 import Image from "next/image";
 
-export default function EditReview({params}:{params:{rid:string}}) {
-   const {data: session} = useSession()
-    if (!session || !session.user.token ) return null
-    console.log(params.rid);
-    
+export default function EditReview({ params }: { params: { rid: string } }) {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  if (!session || !session.user.token) return null;
+  console.log(params.rid);
+
   const [hasEdit, setHasEdit] = useState(false);
   const [data, setData] = useState({
     rating: -1,
     comment: "",
-    
   });
 
   const updateReview = async () => {
+    if (data.rating || data.comment) {
+      const item: Partial<ReviewItemEdit> = {};
+      if (data.rating !== -1) {
+        item.rating = data.rating;
+      }
+      if (data.comment !== "") {
+        item.comment = data.comment;
+      }
 
-    if (data.rating || data.comment ) {
-      const item: Partial<ReviewItemEdit> = {}
-        if (data.rating !== -1) {
-          item.rating = data.rating
-        }
-        if (data.comment !== "") {
-          item.comment = data.comment
-        }
-        
-      console.log(item)
-            
-            const editing = await editReview(session.user.token, params.rid,item);
-            console.log("Booking result:", editing);
-            if (editing.success == true) {
-               setHasEdit(true)
-            }
-            else if (editing.success == false) {
-                alert(editing.message)
-            }
+      console.log(item);
+
+      const editing = await editReview(session.user.token, params.rid, item);
+      console.log("Booking result:", editing);
+      if (editing.success == true) {
+        setHasEdit(true);
+        router.refresh();
+      } else if (editing.success == false) {
+        alert(editing.message);
+      }
     }
   };
 
-  const [comment, setcomment] = useState<string>("")
-  const [rating, setRating] = useState<Number|0>(0)
+  const [comment, setcomment] = useState<string>("");
+  const [rating, setRating] = useState<Number | 0>(0);
   const [value, setValue] = React.useState<number | null>(0);
-
 
   return (
     <main className="mt-8 w-auto items-center content-center place-content-center flex justify-center place-items-center w-[100%]">
-      
       <div className="bg-white min-h-full w-[70%] rounded-3xl m-8 px-16 py-12 md:px-15 md:mx-20 relative">
-      
         <div className="text-xl font-medium flex flex-row">
-            <Image src={'/img/userlogo.png'} className='h-[100%] mt-11 w-auto mb-auto mt-auto' 
-                          alt='logo' width={0} height={0} sizes='10vh'/>
-            <div className="m-5 ">{session.user.name}</div>
+          <Image
+            src={"/img/userlogo.png"}
+            className="h-[100%] mt-11 w-auto mb-auto mt-auto"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="10vh"
+          />
+          <div className="m-5 ">{session.user.name}</div>
         </div>
         <div className="mt-4 text-xl font-bold leading-9 tracking-tight text-gray-900">
-              Edit Review
-        </div><br />
-            
-            
-            <div>
-            <Typography component="legend" className="flex flex-row">Tap to rate</Typography>
-            <Rating
-                
-                size="large"
-                name="rate"
-                value={value}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                    if (newValue !== null) {
-                        setRating(newValue)
-                        
-                    }else{
-                     newValue=0
-                    }
-                    setData({ ...data, rating: newValue.valueOf() });
-                    console.log("this " + newValue);
-                }}
-                
-            />
-            </div><br />
-            <div>Comment</div>
-            <form className="border-2 mb-10 w-[100%] rounded-lg p-5">
-               <TextField
-                     className="m-5 p-10 py-5 w-[100%]"
-                     variant='standard'
-                     margin="normal"
-                     fullWidth
-                     label="Tell us more"
-                     id="comment"
-                     name="comment"
-                     value={comment}
-                     placeholder="Write this"
-                     onChange={(e) => {
-                        setcomment(e.target.value);
-                        setData({ ...data, comment: e.target.value });
-                     }}
-                />
-                
-              </form>
-              <div>
-                  <button
-                  className="flex w-[40%] m-auto justify-center  rounded-md bg-[#252645] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-gradient-to-r hover:from-[#252645] hover:to-[#5C5EAB]"
-                  type="submit" onClick={updateReview}
-                  >
-                    {hasEdit ? (
-                      <Link href={`/mybooking`}>
-                          complete
-                      </Link>
-                    ) : "Edit"}
-                  </button>
-              </div>
-            
-          </div>
+          Edit Review
+        </div>
+        <br />
+
+        <div>
+          <Typography component="legend" className="flex flex-row">
+            Tap to rate
+          </Typography>
+          <Rating
+            size="large"
+            name="rate"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+              if (newValue !== null) {
+                setRating(newValue);
+              } else {
+                newValue = 0;
+              }
+              setData({ ...data, rating: newValue.valueOf() });
+              console.log("this " + newValue);
+            }}
+          />
+        </div>
+        <br />
+        <div>Comment</div>
+        <form className="border-2 mb-10 w-[100%] rounded-lg p-5">
+          <TextField
+            className="m-5 p-10 py-5 w-[100%]"
+            variant="standard"
+            margin="normal"
+            fullWidth
+            label="Tell us more"
+            id="comment"
+            name="comment"
+            value={comment}
+            placeholder="Write this"
+            onChange={(e) => {
+              setcomment(e.target.value);
+              setData({ ...data, comment: e.target.value });
+            }}
+          />
+        </form>
+        <div>
+          <button
+            className="flex w-[40%] m-auto justify-center  rounded-md bg-[#252645] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-gradient-to-r hover:from-[#252645] hover:to-[#5C5EAB]"
+            type="submit"
+            onClick={updateReview}
+          >
+            {hasEdit ? <Link href={`/mybooking`}>complete</Link> : "Edit"}
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
