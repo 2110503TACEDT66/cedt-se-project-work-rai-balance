8d1794fa478bb73d1f895c2cb881ebc7ef776299
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-04-19 02:36:18 +0700

    change router

--- frontend/src/app/mybooking/page.tsx
+++ frontend/src/app/mybooking/page.tsx
@@ -1,25 +1,27 @@
-"use client"
+// 'use client'
+import Link from "next/link"
+import { BookingItem2, BookingJson } from "../../../interface"
+import AllBooking from "@/components/AllBooking"
+import getBookings from "@/libs/getBookings"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import getReviewForReservation from "@/libs/getReviewforReservation"
+import deleteBooking from "@/libs/deleteBooking";
 import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
-import AllBooking from '@/components/AllBooking';
-import getBookings from '@/libs/getBookings';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { useSession } from "next-auth/react"
 
-export default function MyBooking() {
-  const router = useRouter();
+export default async function MyBooking() {
+    // const router = useRouter();
 
-  const { data: session, status } = useSession();
-  
-  if (status === 'loading') return <div>Loading...</div>;
-  if (!session || !session.user.token) return null;
-
-  const bookings = getBookings(session.user.token);
+    const session = await getServerSession(authOptions)
+    if (!session || !session.user.token) return null
     
-
-  return (
-    <main>
-      <AllBooking bookingsJson={bookings} />
-    </main>
-  );
+    const bookings = getBookings(session.user.token)
+    // const reviews = getReviewForReservation("661ad6b69586c1cdfb9a0113")
+    
+    return (
+        <main>
+            <AllBooking bookingsJson={bookings}/>
+        </main>
+    )
 }
