bdb038083ef17a8b313ffe2adc1f1cd866b0ab3a
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-04-27 15:45:51 +0700

    edit review status

--- frontend/src/libs/editReviewStatus.tsx
+++ frontend/src/libs/editReviewStatus.tsx
@@ -1,22 +1,22 @@
-// import { ReviewItemEdit } from "../../interface"
+// // import { ReviewItemEdit } from "../../interface"
 
-import { ApproveReviewEdit } from "interface"
+// // import { ApproveReviewEdit } from "interface"
 
-export default async function editReviewStatus(token: string, reviewId: string, status:string){
-   const response = await fetch(`${process.env.BACKEND_URL}/api/project/reviews/${reviewId}/approve`,{
-      cache: "no-store",
-       method: "PUT",
-       headers: {
-         authorization: `Bearer ${token}`,
-       },
-       body: JSON.stringify({
-        approval:status
-       })
-    })
+// export default async function editReviewStatus(token: string, reviewId: string, status:string){
+//    const response = await fetch(`${process.env.BACKEND_URL}/api/project/reviews/${reviewId}/approve`,{
+//       cache: "no-store",
+//        method: "PUT",
+//        headers: {
+//          authorization: `Bearer ${token}`,
+//        },
+//        body: JSON.stringify({
+//         approval:status
+//        })
+//     })
     
-    if(!response.ok){
-      return await response.json()
-    }
+//     if(!response.ok){
+//       return await response.json()
+//     }
 
-    return await response.json()
-}
+//     return await response.json()
+// }
