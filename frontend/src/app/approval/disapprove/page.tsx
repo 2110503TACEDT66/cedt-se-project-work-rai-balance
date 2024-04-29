012c1c109cdcd6484b1c43f2e3882c2eb2710677
Nawakorn23 <nawakorn.c@gmail.com>
2024-04-29 23:52:13 +0700

    edit disapprove and editApprove

--- frontend/src/app/approval/disapprove/page.tsx
+++ frontend/src/app/approval/disapprove/page.tsx
@@ -1,67 +1,80 @@
-// app/approval/page.tsx
-import { GetServerSideProps } from 'next';
-import { getSession } from "next-auth/react"
-import AllApproval from '@/components/AllApproval';
-import { ApproveItemEdit, ApproveJson } from 'interface';
-import { getServerSession } from "next-auth"
+"use client";
+import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import approve from '@/libs/approval';
-import { ApproveItem } from 'interface';
-import { FormEvent, useState } from "react";
-import Link from 'next/link';
+import deleteBooking from "@/libs/deleteBooking";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import editApprove from "@/libs/editApprove";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import dayjs, { Dayjs } from "dayjs";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/redux/store";
+import { addBooking } from "@/redux/features/bookingSlice";
+import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
+import { DatePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { BookingItemEdit, ReviewItemCoworking } from "../../../../../interface";
+import editBooking from "@/libs/editBooking";
+import approve from "@/libs/approval";
+import getOneReview from "@/libs/getOneReview";
 
-export default async function DisapprovalPage() {
-    const session = await getServerSession(authOptions)
-    if (!session || !session.user.token) return null
-   
-    
-    const item: ApproveItemEdit = {
-        approval : ""
+export default function ApproveReview({ params }: { params: { rid: string } }) {
+  const { data: session } = useSession();
+  if (!session || !session.user.token) return null;
+  const router = useRouter();
+  const urlParams = useSearchParams();
+
+  const [hasApprove, setHasApprove] = useState(false);
+
+  const disapprove = async () => {
+    const disapproveReview = await editApprove(
+      session.user.token,
+      params.rid,
+      "disapproved"
+    );
+    console.log("result:", disapproveReview);
+
+    if (disapproveReview.success == true) {
+      setHasApprove(true);
+    } else if (disapproveReview.success == false) {
+      alert(disapproveReview.message);
     }
+  };
 
-    const approves = await approve(session.user.token,item,"disapproved")
-    return(
-        <main>
-            <div className="flex flex-row z-10 justify-center">
-            <div className="bg-white h-20 mt-4 px-5 hover:bg-slate-200">       
-                    <Link href={'/approval'} className="">
-                        <div className="text-xl font-semibold p-5">
-                        All
-                        </div>
-                        {/* <button className="h-full bg-black text-left font-semibold font-sans w-3/4 text-white text-9xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] shadow-sm bg-[#252645] bg-gradient-to-r hover:from-[#000000] hover:to-[#21D375]">Approve </button> */}
-                    </Link>
-                </div>
-                <div className="bg-white h-20 mt-4 px-5 hover:bg-slate-200">       
-                    <Link href={'/approval/approve'} className="">
-                        <div className="text-xl font-semibold p-5">
-                        Approve
-                        </div>
-                        {/* <button className="h-full bg-black text-left font-semibold font-sans w-3/4 text-white text-9xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] shadow-sm bg-[#252645] bg-gradient-to-r hover:from-[#000000] hover:to-[#21D375]">Approve </button> */}
-                    </Link>
-                </div>
-                <div className="bg-white h-20 mt-4 px-5 hover:bg-slate-200">
-                    <Link href={'/approval/pending'} className="">
-                        <div className="text-xl font-semibold p-5">
-                        Pending
-                        </div>
-                        
-                    </Link>
-                </div>
-                
-                <div className="bg-slate-200 h-20 mt-4 hover:bg-slate-200">
-                    <Link href={'/approval/disapprove'} className="">
-                        <div className="text-xl font-semibold p-5">
-                            Disapprove
-                        </div>
-                        
-                    </Link>
-                </div>
-            </div>
-            <hr />
-            <div className="bg-white ">
-                <AllApproval approvesJson={approves}/>
-            </div>
-            
-        </main>
-    )
-};
+  const refresh = async () => {
+    router.replace("/approval");
+    router.refresh();
+  };
+  useEffect(() => {
+    // Call the approve function when the component mounts
+    disapprove();
+  }, []); // Empty dependency array to ensure this effect runs only once
+
+  return (
+    <main>
+      <div className="flex min-h-full w-auto flex-1 flex-col justify-center rounded-3xl px-6 py-12 md:px-15 md:mx-20 lg:mx-[200px]">
+        <div className="bg-white p-5 rounded-3xl drop-shadow-xl w-auto">
+          <div className="text-xl text-center text-gray-600 m-5 p-5">
+            Disapprove Successfully
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          {/* <Link href={'/approval'}> */}
+          <button
+            onClick={refresh}
+            className="block rounded-md bg-black hover:bg-indigo-900 px-3 py-2 text-white shadow-sm flex flex-row m-10"
+          >
+            Back to Menu
+          </button>
+          {/* </Link> */}
+        </div>
+      </div>
+    </main>
+  );
+}
